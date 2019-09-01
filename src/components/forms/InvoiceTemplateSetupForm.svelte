e999dbb1de52c9e72471beb66b4d257cdeab2b10
Anej Gorkič <anej.gorkic@poviolabs.com>
2019-09-01 23:53:55 +0200

    Refactor change handlers, refactor updates, improve tab style

--- src/components/forms/InvoiceTemplateSetupForm.svelte
+++ src/components/forms/InvoiceTemplateSetupForm.svelte
@@ -2,39 +2,42 @@
   import { onMount } from "svelte";
   import { BIRO_SCHEME } from "constants";
   import { ownerData } from "store";
-  import { idbCreate, idbAdd, idbRead, idbUpdate } from "utils";
+  import { idbRead, idbUpdate } from "utils";
 
   let typingTimeout = null;
 
-  function handleSubmit() {
-    idbUpdate("biro_db", "owner", 1, $ownerData)
-      .then(() => idbRead("biro_db", "owner", 1))
-      .then(result => {
-        ownerData.update(() => result);
-      });
+  function handleInputChange() {
+    window.clearTimeout(typingTimeout);
+    $ownerData = $ownerData;
+    typingTimeout = window.setTimeout(function() {
+      idbUpdate("biro_db", "owner", 1, $ownerData)
+        .then(() => idbRead("biro_db", "owner", 1))
+        .then(result => {
+          ownerData.set(result);
+        });
+    }, 500);
   }
 
   function handleUploadChange(event) {
-    if (event.target.files[0]) {
-      const FR = new FileReader();
+    const file = event.target.files[0];
+    if (file) {
+      const fireReader = new FileReader();
 
-      FR.addEventListener("load", function(e) {
+      fireReader.addEventListener("load", function(e) {
         document.getElementById("signature").src = e.target.result;
         ownerData.update(object => {
-          object.issuer_signature = e.target.result;
-          return object;
+          return { ...object, issuer_signature: e.target.result };
         });
         idbUpdate("biro_db", "owner", 1, $ownerData);
       });
 
-      FR.readAsDataURL(event.target.files[0]);
+      fireReader.readAsDataURL(file);
     }
   }
 
   function handleRemoveSignatureClick() {
     ownerData.update(object => {
-      object.issuer_signature = "";
-      return object;
+      return { ...object, issuer_signature: "" };
     });
     idbUpdate("biro_db", "owner", 1, $ownerData);
   }
@@ -58,19 +61,23 @@
   }
 </style>
 
-<form on:submit|preventDefault={handleSubmit}>
+<form>
   <h3>Invoice template setup</h3>
 
   <div class="field">
     <input
       type="text"
       bind:value={$ownerData.company_name}
+      on:keyup={handleInputChange}
+      on:change={handleInputChange}
       placeholder="Company name" />
   </div>
   <div class="field">
     <input
       type="text"
       bind:value={$ownerData.company_address}
+      on:keyup={handleInputChange}
+      on:change={handleInputChange}
       placeholder="Address" />
   </div>
   <div class="g-r">
@@ -79,6 +86,8 @@
         <input
           type="text"
           bind:value={$ownerData.company_postal_number}
+          on:keyup={handleInputChange}
+          on:change={handleInputChange}
           placeholder="Postal number" />
       </div>
     </div>
@@ -87,6 +96,8 @@
         <input
           type="text"
           bind:value={$ownerData.company_city}
+          on:keyup={handleInputChange}
+          on:change={handleInputChange}
           placeholder="City" />
       </div>
     </div>
@@ -97,6 +108,8 @@
         <input
           type="text"
           bind:value={$ownerData.company_tax_registration_number}
+          on:keyup={handleInputChange}
+          on:change={handleInputChange}
           placeholder="Tax reg. no." />
       </div>
     </div>
@@ -105,6 +118,8 @@
         <input
           type="text"
           bind:value={$ownerData.company_business_registration_number}
+          on:keyup={handleInputChange}
+          on:change={handleInputChange}
           placeholder="Business reg.no." />
       </div>
     </div>
@@ -112,12 +127,22 @@
   <div class="g-r">
     <div class="g-r-c g-r-c-50">
       <div class="field">
-        <input type="text" bind:value={$ownerData.vat} placeholder="VAT" />
+        <input
+          type="text"
+          bind:value={$ownerData.vat}
+          on:keyup={handleInputChange}
+          on:change={handleInputChange}
+          placeholder="VAT" />
       </div>
     </div>
     <div class="g-r-c g-r-c-50">
       <div class="field">
-        <input type="text" bind:value={$ownerData.terms} placeholder="Terms" />
+        <input
+          type="text"
+          bind:value={$ownerData.terms}
+          on:keyup={handleInputChange}
+          on:change={handleInputChange}
+          placeholder="Terms" />
       </div>
     </div>
   </div>
@@ -125,18 +150,24 @@
     <input
       type="text"
       bind:value={$ownerData.pdf_file_name_prefix}
+      on:keyup={handleInputChange}
+      on:change={handleInputChange}
       placeholder="PDF file name prefix" />
   </div>
   <div class="field">
     <input
       type="text"
       bind:value={$ownerData.issue_city}
+      on:keyup={handleInputChange}
+      on:change={handleInputChange}
       placeholder="Issue city" />
   </div>
   <div class="field">
     <input
       type="text"
       bind:value={$ownerData.base_currency}
+      on:keyup={handleInputChange}
+      on:change={handleInputChange}
       placeholder="USD"
       disabled />
   </div>
@@ -144,6 +175,8 @@
     <input
       type="text"
       bind:value={$ownerData.bank_account_number}
+      on:keyup={handleInputChange}
+      on:change={handleInputChange}
       placeholder="Bank account number" />
   </div>
   <div class="field">
